c463252142350f1656c80a13427bc0140e068c8d
kudryavcevanatallia <kudryavceva.natallia@gmail.com>
2025-06-19 17:45:01 +0300

    add fcts

--- models/staging/flights/stg_flights__bookings.sql
+++ models/staging/flights/stg_flights__bookings.sql
@@ -1,9 +1,11 @@
 {{
     config(
-        materialized = 'table'
+        materialized = 'materialized_view'
         )
 }}
 select
-    book_ref, book_date, total_amount
+    book_ref,
+    book_date,
+    total_amount    
 from
     {{ source('demo_src', 'bookings') }}
