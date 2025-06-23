80c4c8f22090e4df2cc77a9d1b5e02880a93c47c
kudryavcevanatallia <kudryavceva.natallia@gmail.com>
2025-06-23 17:33:40 +0300

    add model for incremental refresh

---
+++ models/staging/flights/stg_flights__flights_merge.sql
@@ -0,0 +1,16 @@
+{{
+    config(
+        materialized = 'incremental',
+        unique_key = 'flight_id',
+        incremental_strategy = 'merge',
+        tags = ['flights']
+        )
+}}
+select
+    flight_id, flight_no, scheduled_departure, scheduled_arrival, departure_airport, arrival_airport, status, aircraft_code, actual_departure, actual_arrival
+from
+    {{ source('demo_src', 'flights') }}
+{% if is_incremental () %}
+where scheduled_departure < (select max(scheduled_departure) from  {{ source('demo_src', 'flights') }}) - interval '100 day'
+{% endif %}
+
